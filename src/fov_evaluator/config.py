133f6172d8f135402d368c3c1ee1d0df7fffd695
Cullen Sharp <cullen.sharp100@gmail.com>
2026-04-01 11:42:02 -0700

    Add stubs for estimate and db configs

--- src/fov_evaluator/config.py
+++ src/fov_evaluator/config.py
@@ -8,7 +8,15 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class GenerateArgs:
-    """A conveinance object for working with command line arguments."""
+    """Configuration object for generating synthetic input.
+
+    orientation params
+    declination: [-90 deg, 90 deg] --> [-pi/2 rad, pi/2 rad]
+                where negative means south
+                of the celestial equator and positive means north
+    right ascenion: [0 deg, 360 deg] --> [0 rad, 2pi rad ]
+    roll: [0 rad, 2 pi rad]ms
+    """
 
     false_stars: int = 50
     zero_mag_photons: int = 20_000
@@ -16,16 +24,9 @@ class GenerateArgs:
     dark_current: float = 0.25
     exposure: float = 0.2
     read_noise: float = 0.02
-
-    """
-    orientation params
-    declination: [-90 deg, 90 deg] --> [-pi/2 rad, pi/2 rad]
-                 where negative means south
-                 of the celestial equator and positive means north
-    right ascenion: [0 deg, 360 deg] --> [0 rad, 2pi rad ]
-    roll: [0 rad, 2 pi rad]ms
-    """
     fov: int = 30
+
+    # orientation params
     ra: int = 88
     de: int = 0
     roll: int = 7
@@ -39,3 +40,13 @@ class GenerateArgs:
         name = f"fov{self.fov}ra{self.ra}roll{self.roll}de{self.de}.png"
         logger.debug(name)
         return name
+
+
+@dataclass
+class DatabaseArgs:
+    """Configuration object for generating databases."""
+
+
+@dataclass
+class EstimateArgs:
+    """Configuration object for generating estimates."""
